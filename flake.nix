51fb8a9637312306154be12c6e38679b1aeb5232
dhess-dev <danielhess.dev@gmail.com>
2025-01-28 11:34:45 +0100

    feat: add home-manager

--- flake.nix
+++ flake.nix
@@ -3,17 +3,27 @@
 
    inputs = {
      nixpkgs.url = "nixpkgs/nixos-unstable";
-   };
+     home-manager.url = "github:nix-community/home-manager";
+     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+     };
 
    outputs = { self, nixpkgs, ...}:
     let
      lib = nixpkgs.lib;
+     system = "x86_64-linux";
+     pkgs = nixpkgs.legacyPackages.${system};
     in {
       nixosConfigurations = {
        nixos = lib.nixosSystem {
-       system = "x86_64-linux";
+       inherit system;
        modules = [ ./configuration.nix ]; 
+       };
      };
-    };
-};
+       homeConfiguration = {
+        dhess = home-manager.lib.homeMangerConfiguration {
+        inherit pkgs;
+        modules = [ ./home.nix ];
+        };
+       };
+   };
 }
