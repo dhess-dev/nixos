ed7c525b5a5308f354b11737b9c6f93b8137e5ee
dhess-dev <danielhess.dev@gmail.com>
2025-02-04 14:18:01 +0100

    feat: add sdswitch

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -87,4 +87,6 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+  # automatically reload system units when changing configs
+  systemd.user.startServices = "sd-switch";
 }
